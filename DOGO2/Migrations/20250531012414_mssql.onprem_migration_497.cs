56b49e3d3fa2eaa09584adf366082301cb12ed42
EDB EDB <baezedu@gmail.com>
2025-06-28 22:16:20 -0300

    1stSolution

---
+++ DOGO2/Migrations/20250531012414_mssql.onprem_migration_497.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DOGO2.Migrations
+{
+    /// <inheritdoc />
+    public partial class mssqlonprem_migration_497 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Aviones1",
+                columns: table => new
+                {
+                    IdAvion = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IdTipoAvion = table.Column<int>(type: "int", nullable: false),
+                    IdCategoriaAvion = table.Column<int>(type: "int", nullable: false),
+                    Matricula = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modelo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aviones1", x => x.IdAvion);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aviones1");
+        }
+    }
+}
---
+++ DOGO2/Program.cs
@@ -0,0 +1,43 @@
+﻿using DOGO2.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using DOGO2.Data;
+using Syncfusion.Blazor;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContextFactory<DOGO2Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DOGO2Context") ?? throw new InvalidOperationException("Connection string 'DOGO2Context' not found.")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddSyncfusionBlazor();
+
+var app = builder.Build();
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzkxMDE0OUAzMjM5MmUzMDJlMzAzYjMyMzkzYlhDNVFKa05GQkcwRGIxVjdzVWFRM3p0SU84TWYvMUJiLzZDNnlaN3FpS3c9");
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+    app.UseMigrationsEndPoint();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
