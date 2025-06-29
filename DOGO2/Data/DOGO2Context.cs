b57b537a611c0f4b9011b13fd3892991f4a37a8d
EDB EDB <baezedu@gmail.com>
2025-06-28 22:16:20 -0300

    1stSolution

---
+++ DOGO2/Data/DOGO2Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DOGO2.Model;
+
+namespace DOGO2.Data
+{
+    public class DOGO2Context : DbContext
+    {
+        public DOGO2Context(DbContextOptions<DOGO2Context> options)
+            : base(options)
+        {
+        }
+        public DbSet<Aviones1> Aviones1 { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // No agregues HasNoKey aquí, EF detectará la clave primaria por el atributo [Key] en Aviones1
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ DOGO2/Model/Aviones1.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DOGO2.Model
+{
+    public class Aviones1
+    {
+        [Key]
+        public int IdAvion { get; set; }
+        public int IdTipoAvion { get; set; }
+        public int IdCategoriaAvion { get; set; }
+        public string Matricula { get; set; }
+        public string Nombre { get; set; }
+        public string Modelo { get; set; }
+
+    }
+}
